d389490daa698fe147beaa41e91f777063295306
dyoshino88 <yoshino0707dh@gmail.com>
2023-11-19 07:25:07 +0900

    Carbon

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -9,13 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    エロクアント
+                    エロクアントテスト
                     @foreach ($e_all as $e_owner)
                         {{ $e_owner->name }}
                         {{ $e_owner->created_at->diffForHumans() }}
                     @endforeach
                     <br>
-                    クエリビルダ
+                    クエリビルダテスト
                     @foreach ($q_get as $q_owner)
                         {{ $q_owner->name }}
                         {{ Carbon\Carbon::parse($q_owner->created_at)->diffForHumans() }}

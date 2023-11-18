46f76dfd9fd4915a9c6b224f6916da395e10cef1
dyoshino88 <yoshino0707dh@gmail.com>
2023-11-19 06:28:37 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,14 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; // Eloquent エロクアント
+use Illuminate\Support\Facades\DB; //QueryBuilder クエリビルダー
+use Carbon\Carbon;
 
 class OwnersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
 
     public function __construct()
     {
@@ -20,10 +18,25 @@ class OwnersController extends Controller
 
     public function index()
     {
-        //
-        dd('オーナー一覧です');
+        $data_now = Carbon::now();
+        $data_parse = Carbon::parse(now());
+        echo $data_now;
+        echo $data_parse;
+
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $q_first = DB::table('owners')->select('name')->first();
+
+        // $c_test = collect([
+        //     'name' => 'てすと'
+        // ]);
+
+        // dd($e_all, $q_get, $q_first, $c_test);
+        return view('admin.owners.index',
+        compact('e_all', 'q_get'));
 
     }
+
 
     /**
      * Show the form for creating a new resource.

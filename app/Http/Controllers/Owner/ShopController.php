aa24010a5f8fec2a5a26a61c90e17af19721023f
dyoshino88 <yoshino0707dh@gmail.com>
2023-12-05 23:18:10 +0900

    リサイズなしアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 
 class ShopController extends Controller
@@ -43,12 +44,18 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        dd(shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        // dd(shop::findOrFail($id));
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
-
+        $imageFile = $request->file('image'); //一時保存
+        if(!is_null($imageFile) && $imageFile->isValid() ){
+        Storage::putFile('public/shops', $imageFile);
+        }
+        return redirect()->route('owner.shops.index');
     }
 
 }

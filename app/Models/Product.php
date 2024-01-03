1973fb8215787c547b6de32501ea7196f3c16a2e
dyoshino88 <yoshino0707dh@gmail.com>
2024-01-04 07:32:26 +0900

    stock

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,24 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
+use App\Models\Stock;
 
 
 class Product extends Model
 {
     use HasFactory;
 
-        public function shop()
+    public function shop()
     {
         return $this->belongsTo(shop::class);
     }
 
-        public function category()
+    public function category()
     {
         return $this->belongsTo(SecondaryCategory::class, 'secondary_category_id');
     }
 
-        public function imageFirst()
+    public function imageFirst()
     {
         return $this->belongsTo(Image::class, 'image1', 'id');
+    }
+
+    public function stock()
+    {
+        return $this->hasMany(Stock::class);
     }
 }

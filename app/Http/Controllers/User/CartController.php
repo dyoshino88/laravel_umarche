6fd2ff32e273b9e7b259c5c60f4db7fbed3dc57d
dyoshino88 <yoshino0707dh@gmail.com>
2024-01-28 10:27:50 +0900

    stripe決済テスト

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -62,26 +62,34 @@ class CartController extends Controller
     {
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
-        // Stripeに渡すパラメータを設定
+
         $lineItems = [];
-        foreach($products as $product){
+        foreach($products as $product)
+        {
             $quantity = '';
-            $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+            $quantity = Stock::where('product_id', $product->id)
+            ->sum('quantity');
 
             if($product->pivot->quantity > $quantity){
                 return redirect()->route('user.cart.index');
-            } else {
+            }else{
+                $price_data = ([
+                    'unit_amount' => $product->price,
+                    'currency' => 'jpy',
+                    'product_data' => $product_data = ([
+                        'name' => $product->name,
+                        'description' => $product->information,
+                    ]),
+                ]);
+
                 $lineItem = [
-                'name' => $product->name,
-                'description' => $product->information,
-                'amount' => $product->price,
-                'currency' => 'jpy',
-                'quantity' => $product->pivot->quantity,
-            ];
-            array_push($lineItems, $lineItem);
+                    'price_data' => $price_data,
+                    'quantity' => $product->pivot->quantity,
+                ];
+                array_push($lineItems, $lineItem);
             }
         }
-        // dd($lineItems);
+
         foreach($products as $product){
             Stock::create([
                 'product_id' => $product->id,
@@ -90,13 +98,10 @@ class CartController extends Controller
             ]);
         }
 
-        dd('test');
-
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
-
         $session = \Stripe\Checkout\Session::create([
             'payment_method_types' => ['card'],
-            'line_items' => [$lineItems],
+            'line_items' => [[$lineItems]],
             'mode' => 'payment',
             'success_url' => route('user.items.index'),
             'cancel_url' => route('user.cart.index'),
@@ -104,7 +109,7 @@ class CartController extends Controller
 
         $publicKey = env('STRIPE_PUBLIC_KEY');
 
-        return view('user.checkout',
-        compact('session', 'publicKey'));
+        return view('user.checkout', compact('session', 'publicKey'));
+
     }
 }

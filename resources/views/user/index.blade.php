014a65a16511dbc615bf16a4ab36e3e1e4c151f4
dyoshino88 <yoshino0707dh@gmail.com>
2024-01-29 22:27:27 +0900

    表示順

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,14 +1,55 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            ホーム
-        </h2>
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                商品一覧
+            </h2>
+            <div>
+                <form method="get" action="{{ route('user.items.index') }}">
+                    <div class="flex">
+                        <div>
+                            <span class="text-sm">表示順</span><br>
+                            <select id="sort" name="sort" class="mr-4">
+                                <option value="{{ \Constant::SORT_ORDER['recommend']}}"
+                                    @if(\Request::get('sort') === \Constant::SORT_ORDER['recommend'] )
+                                    selected
+                                    @endif>おすすめ順
+                                </option>
+                                <option value="{{ \Constant::SORT_ORDER['higherPrice']}}"
+                                    @if(\Request::get('sort') === \Constant::SORT_ORDER['higherPrice'] )
+                                    selected
+                                    @endif>料金の高い順
+                                </option>
+                                <option value="{{ \Constant::SORT_ORDER['lowerPrice']}}"
+                                    @if(\Request::get('sort') === \Constant::SORT_ORDER['lowerPrice'] )
+                                    selected
+                                    @endif>料金の安い順
+                                </option>
+                                <option value="{{ \Constant::SORT_ORDER['later']}}"
+                                    @if(\Request::get('sort') === \Constant::SORT_ORDER['later'] )
+                                    selected
+                                    @endif>新しい順
+                                </option>
+                                <option value="{{ \Constant::SORT_ORDER['older']}}"
+                                    @if(\Request::get('sort') === \Constant::SORT_ORDER['older'] )
+                                    selected
+                                    @endif>古い順
+                                </option>
+                            </select>
+
+
+                        </div>
+                        <div>表示件数</div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-sortwhite border-b border-gray-200">
                     <div class="flex flex-wrap">
                         @foreach($products as $product)
                         <div class="w-1/4 p-2 md:p-4">
@@ -29,4 +70,10 @@
             </div>
         </div>
     </div>
+    <script>
+        const select = document.getElementById('sort')
+        select.addEventListener('change', function(){
+            this.form.submit()
+        })
+    </script>
 </x-app-layout>
